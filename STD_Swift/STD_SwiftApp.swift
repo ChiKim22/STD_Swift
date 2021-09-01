8e23693a7f14c04ccba1a50cb7252b274ec35100
ChiKim22 <wlsgh2505@gmail.com>
2021-09-01 11:49:00 +0900

    Initial Commit

---
+++ STD_Swift/STD_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  STD_SwiftApp.swift
+//  STD_Swift
+//
+//  Created by Jinho Kim on 2021/09/01.
+//
+
+import SwiftUI
+
+@main
+struct STD_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
