bb043fb9ff1d0990e2f34f6da542bd2aa5ef207c
ChiKim22 <wlsgh2505@gmail.com>
2021-09-01 15:33:50 +0900

    Swift Map View with Map Kit

---
+++ STD_Swift/MapView.swift
@@ -0,0 +1,27 @@
+//
+//  MapView.swift
+//  STD_Swift
+//
+//  Created by Jinho Kim on 2021/09/01.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(latitude: 36.75, longitude: 137.01667),
+        span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    )
+    
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
